bb04806b76a31c3f1e56edb67228eb498cbeb406
David DeGraw <degraw@fastmail.com>
2024-03-19 16:52:53 -0600

    update

--- src/index.ts
+++ src/index.ts
@@ -7,57 +7,84 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>()
 
-app.get('/badge.svg', async (c) => {
-  try {
-    const source = c.req.header('Referer')?.replace(/\/+$/, '') ?? 'default'
+app.get('*', async (c) => {
+  if (new URL(c.req.url).pathname !== '/') return c.redirect('/', 307)
 
-    c.env.DB.prepare("INSERT OR IGNORE INTO Likes (SourceName, SourceLikes) VALUES (?, 1)")
-      .bind(source).run()
+  c.header('Content-Type', 'image/svg+xml')
+  c.header('Cache-control', 'no-cache')
+  c.header('Access-Control-Allow-Origin', '*')
+  if (c.req.header('Referer')) {
+    try {
+      const source = c.req.header('Referer')?.replace(/\/+$/, '')
+
+      await c.env.DB.prepare("INSERT OR IGNORE INTO Likes (SourceName, SourceLikes) VALUES (?, 1)")
+        .bind(source).run()
+
+      const stmt = c.env.DB.prepare("SELECT SourceLikes FROM Likes WHERE SourceName = ?")
+      const likes = await stmt.bind(source).first('SourceLikes')
+      const text = `${likes} ${likes === 1 ? 'Like' : 'Likes'}`
+      const textWidth = text.length * 8;
+
+      const svg = `
+  			<svg xmlns="http://www.w3.org/2000/svg" height="20" width="${textWidth}">
+  			  <text id="likesText" x="50%" y="50%" font-family="sans-serif" dominant-baseline="middle" text-anchor="middle">${text}</text>
+  			</svg>
+  		`
+      return c.body(svg)
+    } catch (e) {
+      console.log(e)
+
+      const text = `500 Likes`
+      const textWidth = text.length * 8;
+      const svg = `
+        <svg xmlns="http://www.w3.org/2000/svg" height="20" width="${textWidth}">
+          <text id="likesText" x="50%" y="50%" font-family="sans-serif" dominant-baseline="middle" text-anchor="middle">${text}</text>
+        </s
+      `
+      return c.body(svg)
+    }
+  } else {
+    // no referer = browser viewing the page?
+    return c.html(`
+        <img style='cursor: pointer;' src='${c.req.url}' onclick="fetch(this.src, {method:'POST'}).then(()=>this.src=this.src.split('?')[0]+'?t='+Date.now())">
+      `)
+  }
+})
+
+app.post('*', async (c) => {
+  if (new URL(c.req.url).pathname !== '/') return c.redirect('/', 307)
+
+  c.header('Content-Type', 'image/svg+xml')
+  c.header('Cache-control', 'no-cache')
+  c.header('Access-Control-Allow-Origin', '*')
+  try {
+  	const source = c.req.header('Referer')?.replace(/\/+$/, '')?.replace(/\/+$/, '') ?? 'default'
+
+  	c.env.DB.prepare("INSERT OR REPLACE INTO Likes (SourceName, SourceLikes) VALUES (?, COALESCE((SELECT SourceLikes + 1 FROM Likes WHERE SourceName = ?), 2))")
+		.bind(source, source).run()
 
     const stmt = c.env.DB.prepare("SELECT SourceLikes FROM Likes WHERE SourceName = ?")
     const likes = await stmt.bind(source).first('SourceLikes')
     const text = `${likes} ${likes === 1 ? 'Like' : 'Likes'}`
     const textWidth = text.length * 8;
 
-    // Construct the SVG string
     const svg = `
-			<svg xmlns="http://www.w3.org/2000/svg" height="20" width="${textWidth}">
-			  <text id="likesText" x="50%" y="50%" font-family="sans-serif" dominant-baseline="middle" text-anchor="middle">${text}</text>
-			  <script type="text/ecmascript">
-			    <![CDATA[
-			      var textElement = document.getElementById('likesText');
-			      textElement.addEventListener('mouseover', function() {
-			        textElement.textContent = '👍 Like!';
-			      });
-			      textElement.addEventListener('mouseout', function() {
-			        textElement.textContent = '${text}';
-			      });
-			    ]]>
-			  </script>
-			</svg>
-		`
-		c.header('Content-Type', 'image/svg+xml')
+      <svg xmlns="http://www.w3.org/2000/svg" height="20" width="${textWidth}">
+        <text id="likesText" x="50%" y="50%" font-family="sans-serif" dominant-baseline="middle" text-anchor="middle">${text}</text>
+      </svg>
+    `
     return c.body(svg)
   } catch (e) {
-    console.log(e)
-    c.set('Content-Type', 'text/plain')
-    return c.text(500)
-  }
-})
-
-app.post('/', async (c) => {
-  try {
-  	const source = c.req.header('Referer')?.replace(/\/+$/, '') ?? 'default'
-
-  	c.env.DB.prepare("INSERT OR REPLACE INTO Likes (SourceName, SourceLikes) VALUES (?, COALESCE((SELECT SourceLikes + 1 FROM Likes WHERE SourceName = ?), 1))")
-		.bind(source, source).run()
-
-    const stmt = c.env.DB.prepare("SELECT SourceLikes FROM Likes WHERE SourceName = ?")
-    const likes = await stmt.bind(source).first('SourceLikes')
-    return c.text(likes ?? 1)
-  } catch (e) {
   	console.log(e)
-    return c.text(500)
+
+    const text = `500 Likes`
+    const textWidth = text.length * 8;
+    const svg = `
+      <svg xmlns="http://www.w3.org/2000/svg" height="20" width="${textWidth}">
+        <text id="likesText" x="50%" y="50%" font-family="sans-serif" dominant-baseline="middle" text-anchor="middle">${text}</text>
+      </s
+    `
+    return c.body(svg)
   }
 })
 
