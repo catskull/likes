4a0b64230ca85d2ce2e2a7c2ed2d4d946dc9f8b3
David DeGraw <degraw@fastmail.com>
2024-03-19 12:32:37 -0600

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,64 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono'
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+// This ensures c.env.DB is correctly typed
+type Bindings = {
+  DB: D1Database
 }
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.get('/badge.svg', async (c) => {
+  try {
+    const source = c.req.header('Referer')?.replace(/\/+$/, '') ?? 'default'
+
+    c.env.DB.prepare("INSERT OR IGNORE INTO Likes (SourceName, SourceLikes) VALUES (?, 1)")
+      .bind(source).run()
+
+    const stmt = c.env.DB.prepare("SELECT SourceLikes FROM Likes WHERE SourceName = ?")
+    const likes = await stmt.bind(source).first('SourceLikes')
+    const text = `${likes} ${likes === 1 ? 'Like' : 'Likes'}`
+    const textWidth = text.length * 8;
+
+    // Construct the SVG string
+    const svg = `
+			<svg xmlns="http://www.w3.org/2000/svg" height="20" width="${textWidth}">
+			  <text id="likesText" x="50%" y="50%" font-family="sans-serif" dominant-baseline="middle" text-anchor="middle">${text}</text>
+			  <script type="text/ecmascript">
+			    <![CDATA[
+			      var textElement = document.getElementById('likesText');
+			      textElement.addEventListener('mouseover', function() {
+			        textElement.textContent = '👍 Like!';
+			      });
+			      textElement.addEventListener('mouseout', function() {
+			        textElement.textContent = '${text}';
+			      });
+			    ]]>
+			  </script>
+			</svg>
+		`
+		c.header('Content-Type', 'image/svg+xml')
+    return c.body(svg)
+  } catch (e) {
+    console.log(e)
+    c.set('Content-Type', 'text/plain')
+    return c.text(500)
+  }
+})
+
+app.post('/', async (c) => {
+  try {
+  	const source = c.req.header('Referer')?.replace(/\/+$/, '') ?? 'default'
+
+  	c.env.DB.prepare("INSERT OR REPLACE INTO Likes (SourceName, SourceLikes) VALUES (?, COALESCE((SELECT SourceLikes + 1 FROM Likes WHERE SourceName = ?), 1))")
+		.bind(source, source).run()
+
+    const stmt = c.env.DB.prepare("SELECT SourceLikes FROM Likes WHERE SourceName = ?")
+    const likes = await stmt.bind(source).first('SourceLikes')
+    return c.text(likes ?? 1)
+  } catch (e) {
+  	console.log(e)
+    return c.text(500)
+  }
+})
+
+export default app
